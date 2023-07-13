8a5793a752d2ffee17db60ffb267ca4b62baf545
yezehao <sgzye5@liverpool.ac.uk>
2023-07-13 10:34:18 +0100

    add mysql file for debug

---
+++ pris_mysql/pris_mysql/spiders/debug-mysql.sql
@@ -0,0 +1,6 @@
+SELECT * FROM `pris`.`pris_country` LIMIT 100;
+-- TRUNCATE TABLE `pris`.`pris_country`;
+-- ALTER TABLE `pris`.`pris_country` MODIFY COLUMN country VARCHAR(50);
+
+-- ALTER TABLE `pris`.`pris_type`
+-- ADD COLUMN `country` VARCHAR(50) AFTER `type`;
