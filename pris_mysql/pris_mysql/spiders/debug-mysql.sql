e227033c2d647a67a2bcd04e143218ad1a6d3dcb
yezehao <sgzye5@liverpool.ac.uk>
2023-07-14 04:01:00 +0100

    update the debug mysql command

--- pris_mysql/pris_mysql/spiders/debug-mysql.sql
+++ pris_mysql/pris_mysql/spiders/debug-mysql.sql
@@ -1,6 +1,13 @@
-SELECT * FROM `pris`.`pris_country` LIMIT 100;
+-- SELECT * FROM `pris`.`pris_region` LIMIT 100;
 -- TRUNCATE TABLE `pris`.`pris_country`;
 -- ALTER TABLE `pris`.`pris_country` MODIFY COLUMN country VARCHAR(50);
 
 -- ALTER TABLE `pris`.`pris_type`
 -- ADD COLUMN `country` VARCHAR(50) AFTER `type`;
+
+USE pris;
+SELECT COUNT(*) AS row_count FROM pris_age;
+SELECT COUNT(*) AS row_count FROM pris_country;
+SELECT COUNT(*) AS row_count FROM pris_region;
+SELECT COUNT(*) AS row_count FROM pris_trend;
+SELECT COUNT(*) AS row_count FROM pris_type;
