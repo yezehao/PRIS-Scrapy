9581b7b6dbc1193ef142326b984c841158045989
yezehao <sgzye5@liverpool.ac.uk>
2023-07-17 03:59:05 +0100

    modify sql command

--- pris_mysql/pris_mysql/spiders/debug-mysql.sql
+++ pris_mysql/pris_mysql/spiders/debug-mysql.sql
@@ -1,13 +1,19 @@
--- SELECT * FROM `pris`.`pris_region` LIMIT 100;
+USE pris;
+SELECT * FROM `pris_age` LIMIT 100;
+SELECT * FROM `pris_country` LIMIT 100;
+SELECT * FROM `pris_region` LIMIT 100;
+SELECT * FROM `pris_trend` LIMIT 100;
+SELECT * FROM `pris_type` LIMIT 100;
+
 -- TRUNCATE TABLE `pris`.`pris_country`;
 -- ALTER TABLE `pris`.`pris_country` MODIFY COLUMN country VARCHAR(50);
 
 -- ALTER TABLE `pris`.`pris_type`
 -- ADD COLUMN `country` VARCHAR(50) AFTER `type`;
 
-USE pris;
-SELECT COUNT(*) AS row_count FROM pris_age;
-SELECT COUNT(*) AS row_count FROM pris_country;
-SELECT COUNT(*) AS row_count FROM pris_region;
-SELECT COUNT(*) AS row_count FROM pris_trend;
-SELECT COUNT(*) AS row_count FROM pris_type;
+-- USE pris;
+-- SELECT COUNT(*) AS row_count FROM pris_age;
+-- SELECT COUNT(*) AS row_count FROM pris_country;
+-- SELECT COUNT(*) AS row_count FROM pris_region;
+-- SELECT COUNT(*) AS row_count FROM pris_trend;
+-- SELECT COUNT(*) AS row_count FROM pris_type;
